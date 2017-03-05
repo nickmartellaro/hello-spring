0e9247c858831d4928e17d5d1fb91259a29d6104
Nick Martellaro <nick.martellaro@gmail.com>
2017-03-05 12:51:18 -0600

    finished walkthrough

--- src/main/java/com/company/controllers/HelloController.java
+++ src/main/java/com/company/controllers/HelloController.java
@@ -1,9 +1,12 @@
 package com.company.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.multipart.support.AbstractMultipartHttpServletRequest;
 
 
 /**
@@ -15,14 +18,51 @@ public class HelloController {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index() {
-        return "Hello World";
+    public String index(HttpServletRequest request) {
+        String name = request.getParameter("name");
+
+        if (name == null) {
+            name = "World";
+        }
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloForm() {
+
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!'/>" +
+                "</form>";
+
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request) {
+        String name = request.getParameter("name");
+
+        return "hello " + name;
+
+    }
+
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+
+    public String helloUrlSegment(@PathVariable String name) {
+        return "hello " + name;
+
     }
 
     @RequestMapping(value = "goodbye")
-    @ResponseBody
+
     public String goodbye() {
-        return "Goodbye";
-        
+
+        return "redirect:/";
+
     }
 }
